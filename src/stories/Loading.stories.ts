b1c728f6c19b891301499ab549e9962c9b185e9d
Makson19 <makson.douglas@gmail.com>
2026-03-17 09:54:50 -0300

    refactor: quick adjustments

--- src/stories/Loading.stories.ts
+++ src/stories/Loading.stories.ts
@@ -11,14 +11,18 @@ const meta = {
       control: 'color'
     },
     size: {
-      description: 'Defines the size of the loading indicator (applicable for circular variant).',
+      description: 'Defines the size of the loading indicator (applicable for circular variant). <code>1 = 1px</code>',
       control: {
         type: 'number',
         min: 0
       }
     },
     thickness: {
-      description: 'Defines the thickness of the loading indicator.',
+      description: 'Defines the thickness of the loading indicator. <code>1 = 1px</code>',
+      control: {
+        type: 'number',
+        min: 0
+      }
     },
     variant: {
       description: 'Defines the visual style of the loading indicator: a spinning circle (<code>circular</code>) or a horizontal bar (<code>linear</code>).'
